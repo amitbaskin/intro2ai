0a520bcef58f889f00d2fd17a62263fbecb244e7
CanzonrLaLa <ronitzhakk1993@gmail.com>
2020-04-04 15:33:08 +0300

    added astar search, cannot be tested without blokus being implemented first

--- blokus/search.py
+++ blokus/search.py
@@ -4,6 +4,7 @@ In search.py, you will implement generic search algorithms
 
 import util
 from collections import deque
+from queue import PriorityQueue
 
 
 class SearchProblem:
@@ -54,13 +55,17 @@ class GraphNode:
     originator_node = None
     state = None
     move = None
-    cost = None
+    cost = 0
+    heuristic_cost = 1
+    cost_so_far = 0
 
-    def __init__(self, origin_node, state, move, cost):
+    def __init__(self, origin_node, state, move, cost, heuristic_cost=1, cost_so_far=0):
         self.originator_node = origin_node
         self.state = state
         self.move = move
         self.cost = cost
+        self.heuristic_cost = heuristic_cost
+        self.cost_so_far = cost_so_far
 
     def get_moves(self):
         # Get steps by going up the path to origin
@@ -71,6 +76,18 @@ class GraphNode:
             curr_node = curr_node.originator_node
         steps.reverse()
         return steps
+
+    def overall_cost(self):
+        return self.heuristic_cost + self.cost_so_far
+
+    def __eq__(self, other):
+        return self.overall_cost() == other.overall_cost()
+
+    def __gt__(self, other):
+        return self.overall_cost() > other.overall_cost()
+
+    def __lt__(self, other):
+        return self.overall_cost() < other.overall_cost()
 
 
 def graph_search_pattern(problem, insertion_func):
@@ -98,7 +115,6 @@ def graph_search_pattern(problem, insertion_func):
             insertion_func(legal_action_triplets, fringe, node)
 
     return steps
-
 
 
 def generic_search_pattern(problem, insertion_func):
@@ -138,13 +154,15 @@ def depth_first_search(problem):
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
 
-	print("Start:", problem.get_start_state().state)
+    print("Start:", problem.get_start_state().state)
     print("Is the start a goal?", problem.is_goal_state(problem.get_start_state()))
     print("Start's successors:", problem.get_successors(problem.get_start_state()))
     """
+
     def dfs_graph_insertion_func(legal_action_triplets, fringe, curr_node):
         for triplet in legal_action_triplets:
             fringe.insert(0, GraphNode(curr_node, triplet[0], triplet[1], triplet[2]))
+
     def dfs_insertion_func(fringe, legal_action_triplets, prev_actions):
         for triplet in legal_action_triplets:
             fringe.insert(0, (prev_actions.copy(), triplet))
@@ -156,9 +174,11 @@ def breadth_first_search(problem):
     """
     Search the shallowest nodes in the search tree first.
     """
+
     def bfs_graph_insertion_func(legal_action_triplets, fringe, curr_node):
         for triplet in legal_action_triplets:
             fringe.append(GraphNode(curr_node, triplet[0], triplet[1], triplet[2]))
+
     def bfs_insertion_func(fringe, legal_action_triplets, prev_actions):
         for triplet in legal_action_triplets:
             fringe.insert(-1, (prev_actions.copy(), triplet))
@@ -182,13 +202,43 @@ def null_heuristic(state, problem=None):
     return 0
 
 
+def graph_search_pattern_for_a_star(fringe, problem, heuristic=null_heuristic):
+    visited_list = set()
+    start_state = problem.get_start_state()
+    start_node = GraphNode(None, start_state, None, None, heuristic(start_state, problem))
+    steps = []
+
+    legal_action_triplets = problem.get_successors(start_state)
+
+    for triplet in legal_action_triplets:
+        fringe.put(GraphNode(start_node, triplet[0], triplet[1], triplet[2],
+                             heuristic_cost=heuristic(start_state, problem), cost_so_far=triplet[2]))
+    visited_list.add(start_state)
+    while fringe:
+        node = fringe.get()
+        if node.state in visited_list:
+            continue
+        else:
+            visited_list.add(node.state)
+        if problem.is_goal_state(node.state):
+            steps = node.get_moves()
+            break
+        else:
+            legal_action_triplets = problem.get_successors(node.state)
+            for triplet in legal_action_triplets:
+                fringe.put(GraphNode(node, triplet[0], triplet[1], triplet[2],
+                                     heuristic_cost=heuristic(start_state, problem),
+                                     cost_so_far=triplet[2] + node.cost_so_far))
+
+    return steps
+
+
 def a_star_search(problem, heuristic=null_heuristic):
     """
     Search the node that has the lowest combined cost and heuristic first.
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
+    fringe_queue = PriorityQueue()
+    return graph_search_pattern_for_a_star(fringe_queue, problem, heuristic)
 
 
 # Abbreviations
