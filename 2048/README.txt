4b72eca37290e5edcedaccaee4b27b7e12a3ad01
CanzoneLaLa <ronitzhakk1993@gmail.com>
2020-05-23 18:35:35 +0300

    finish 2048, 60% theortical

--- 2048/README.txt
+++ 2048/README.txt
@@ -1,4 +1,22 @@
-12345678
-87654321
+311604938
+312259013
 *****
 Comments:
+To make sure that the board stays in a state that is conducive to merges we made a few heuristics:
+1. The board is monotone in a direction, this means that the highest tile is more likely to stay at a corner and the
+   further away you get from it, the smaller the numbers get, this means that merges of small numbers happen in one
+   corner of the board, and big merges happen in the opposite side. We ignore empty tiles that are in between the
+   tiles.
+2. To make sure tiles stay in places where the can be merged, we want adjacent tiles to be similar. We ignore empty
+   tiles that are in between the tiles.
+3. We want to make sure that there are places in which tiles can move, so we want to maximize the number of empty
+   tiles.
+4. We obviously want the game to have the highest tile possible.
+5. Also for the sum  of the tiles on board.
+6. We want to make extra sure that the biggest tile is in one of the corners to make sure that this event cannot
+   happen: The biggest tile moves for "one turn" to make something happen, and then a tile spawns in the corner where
+           it would have returned to.
+   And so, we added this heuristic.
+7.  Obviously, the score itself is important as well.
+
+All these heuristics are weighed in a manner such that they are all of the same order of magnitude more or less.
--- 2048/multi_agents.py
+++ 2048/multi_agents.py
@@ -2,6 +2,7 @@ import numpy as np
 import abc
 import util
 from game import Agent, Action
+import math
 
 
 class ReflexAgent(Agent):
@@ -309,21 +310,48 @@ def board_smoothness(board):
     return -score
 
 
+def biggest_num_in_corner(board):
+    highest_tile_locations = np.argwhere(board == np.amax(board))
+    h, w = board.shape
+    for location in highest_tile_locations:
+        if np.all(location == [0, 0]) or np.all(location == [0, w]) or \
+           np.all(location == [h, 0]) or np.all(location == [h, w]):
+            return board.max()
+    return 0
+
+
 def better_evaluation_function(current_game_state):
     """
     Your extreme 2048 evaluation function (question 5).
 
-    DESCRIPTION: <write something here so we know what you did>
-    """
+    DESCRIPTION:
+    To make sure that the board stays in a state that is conducive to merges we made a few heuristics:
+    1. The board is monotone in a direction, this means that the highest tile is more likely to stay at a corner and the
+       further away you get from it, the smaller the numbers get, this means that merges of small numbers happen in one
+       corner of the board, and big merges happen in the opposite side. We ignore empty tiles that are in between the
+       tiles.
+    2. To make sure tiles stay in places where the can be merged, we want adjacent tiles to be similar. We ignore empty
+       tiles that are in between the tiles.
+    3. We want to make sure that there are places in which tiles can move, so we want to maximize the number of empty
+       tiles.
+    4. We obviously want the game to have the highest tile possible.
+    5. Also for the sum  of the tiles on board.
+    6. We want to make extra sure that the biggest tile is in one of the corners to make sure that this event cannot
+       happen: The biggest tile moves for "one turn" to make something happen, and then a tile spawns in the corner
+               where it would have returned to.
+    7. Obviously, the score itself is important as well.
 
+    All these heuristics are weighed in a manner such that they are all of the same order of magnitude more or less.
+    """
     monotonicity = board_monotonicity(current_game_state.board) * 150
     smoothness = board_smoothness(current_game_state.board) * 10
     empty_tiles = np.sum(current_game_state.board == 0) * 200
     max_tile = current_game_state.max_tile * 10
-    score = current_game_state.score
     sum_tiles = np.sum(current_game_state.board) * 15
+    in_corner = biggest_num_in_corner(current_game_state.board) * 10
+    score = current_game_state.score
 
-    return monotonicity + score + smoothness + empty_tiles + max_tile + sum_tiles
+    return monotonicity + score + smoothness + empty_tiles + max_tile + sum_tiles + in_corner
     
 
 # Abbreviation
